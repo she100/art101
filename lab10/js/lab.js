a4d37a82722f7ed1d25c85f4681e788e0d78aca9
stxph <she100@ucsc.edu>
2023-06-08 14:49:52 -0700

    a

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,10 +1,43 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - The purpose is to experiment with JavaScript events.
+// INCLUDE
+// Author: Stephanie He
+// Date: May 16, 2023
 
-// Constants
 
-// Functions
+// Button
+
+var button = document.getElementById('my-button');
+
+
+// Function that sorts name
+
+function nameSort(username){
+
+    return username.toLowerCase().split("").sort().join("");
+
+}
+
+// function that takes value of input and runs it through array, then saves the results.
+
+function randomizeName(username){
+    var nameArray = username.toLowerCase().split("");
+    console.log("nameArray =", nameArray);
+
+
+var shuffledArray = shuffleArray(nameArray);
+console.log("shuffledArray =", shuffledArray);
+var shuffledString = shuffledArray.join("");
+
+return newName;
+
+}
+
+
+
+// Replace the HTML in <div id=output> with the results
+
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
