ed24a3e871407eff52d7c10c6e63bfd7feec3616
stxph <she100@ucsc.edu>
2023-06-08 02:09:33 -0700

    everything

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,45 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Rock, Paper, Scissors!
+// Pseudocode for the classic game of rock, paper, and scissors.
+// Author: Stephanie He <she100@ucsc.edu>
+// Date: April 25, 2023
+
+
+// Pseudocode starts here!
+
+// Define the variables: rock, paper, and scissors
+
+// Print: "Let's play rock, paper, and scissors! Type a number."
+
+// Defining rock (1)
+    // If faced with paper, defeat
+    // If faced with scissors, victory
+    // If faced with rock, draw
+
+// Defining paper (2)
+    // If faced with scissors, defeat
+    // If faced with rock, victory
+    // If faced with paper, draw
+
+// Defining scissors (3)
+    // If faced with paper, victory
+    // If faced with rock, defeat
+    // If faced with scissors, draw
+
+// Loop
+
+// Randomize combinations
+    // If given an integer, randomize the output from 1-3
+    // If input does not equal an integer from 1-3, print "Try again. Type a number."
+  
+// When given an appropriate input, print defeat, victory, or draw.
+// Print "Play Again?"
+
+// End Loop
+
+
+
+
+
 
 // Constants
 
@@ -19,3 +58,5 @@ function main() {
 
 // let's get this party started
 main();
+
+// 
