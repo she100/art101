9211789f0aa42479f877cb0d0f175b090d7d8b55
stxph <she100@ucsc.edu>
2023-06-08 15:50:32 -0700

    a

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -3,6 +3,29 @@
 // Author: Stephanie He
 // Date: May 18, 2023
 
+
+// This is button from jQuery
+$(".sectionbox").append("<button class='btn btn-secondary highlight-button'>Toggle Highlight</button>");
+
+// Click Event for btn-secondary
+$(".btn btn-secondary highlight-button").click(function(){
+  $(this).parent().toggleClass("special");
+})
+
+
+
+
+
+
+
+// Click event for challenge button
+
+$(".btn").click(function)(){
+
+}
+
+
+
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
@@ -16,10 +39,3 @@ function main() {
 
 // let's get this party started
 main();
-
-
-// Click event for challenge button
-
-$(".hightlight-button").click(function)(){
-
-}
