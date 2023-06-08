f6dddcdd2ae51b1260cfa71efa42e33cbfda0a7a
stxph <she100@ucsc.edu>
2023-06-08 15:36:47 -0700

    a

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,10 +1,7 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
-
-// Functions
+// index.js - the purpose of this lab is to experiment with jQuery.
+// Using JS file to have a button and add an event listener.
+// Author: Stephanie He
+// Date: May 18, 2023
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
@@ -19,3 +16,10 @@ function main() {
 
 // let's get this party started
 main();
+
+
+// Click event for challenge button
+
+$(".hightlight-button").click(function)(){
+
+}
