645a8106bd4ed5239898d1404cbec1e4e537ccd8
stxph <she100@ucsc.edu>
2023-06-08 18:17:55 -0700

    2

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -23,7 +23,6 @@ function sortingHat(str) {
 }
 
 
-
 // This is the results section
 housesArray = [
   {
@@ -43,6 +42,47 @@ housesArray = [
       text: "A Calico Cat type likes to bask under the sun and eat dried fish."
   }
 ]
+
+
+
+// Click listener attached to #button
+var myButton = document.getElementById("button");
+myButton.addEventListener("click", function() {
+
+  // takes value of #input, then assigns it a variable name
+  var name = document.getElementById("input").value;
+
+  // runs the sorting function and stores the result in a variable house
+  var houseObj = sortingHat(name);
+
+  // appends paragraph that says "The Sorting Hat has sorted you into" + cat type
+  newText = "<h2>" + houseObj.title + "!</h2>" +
+          introHTML +
+          "<p>\"" + houseObj.text + "\"</p>" +
+          creditHTML;
+  var outputArea = document.getElementById("output");
+  outputArea.innerHTML = newText;
+  removeClass(outputArea, "hidden");
+  removeClass(document.getElementById("tail-box"), "hidden");
+})
+
+
+// Attach click handler to submit button
+$("#button").click(function(){
+// get the value in the #input text box, assign to a varaiable name
+var name = $("#input").val()
+console.log(name);
+// get the length of the name
+var nameLength = name.length;
+console.log(nameLength);
+// get the cat type
+var type = getType(nameLength);
+console.log(type);
+// add type to output div
+$("#output").html("<h1>" + type + "</h1>");
+})
+
+
 
 
 
