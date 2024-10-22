0713fb0ae219b23a97d1136f67b2cd4049c31bac
stxph <she100@ucsc.edu>
2024-10-21 19:39:49 -0700

    documentwriteln

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -16,6 +16,10 @@ age = 2024 - year;
 
 // Output
 document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color:" + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
 
 // Constants
 
