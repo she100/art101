b332cc6bdefc8504bf45612ccbd92341809ae336
stxph <she100@ucsc.edu>
2024-10-23 00:15:35 -0700

    Update lab.js

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,10 +1,35 @@
 // index.js - Lab 6
 // Experiment with JavaScript arrays and objects.
-// Author: Stephanie
+// Author: Stephanie He
 // Date: October 24, 2024
 
-// Constants
 
+// Define the Variables
+myTransport = ["Metro bus", "car", "friend's car"];
+
+
+// Create an object for my main ride
+myMainRide = {
+  make: "Toyota",
+  model: "RAV4",
+  color: "dark gray",
+  year: 2023,
+  age: 1,
+};
+
+
+// Output
+document.writeln("Here are my methods of transportation that I use: ", myTransport, "</br>");
+// Printing an object to the document
+document.writeln("Here is my Main Ride: <pre>"
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
+
+
+
+
+// Stuff that came with the file automatically
+// Constants
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
