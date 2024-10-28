270b07e8bc49e8f5f6ed71815cd0a70d5e8719d2
stxph <she100@ucsc.edu>
2024-10-28 10:31:48 -0700

    Update lab.js

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -3,8 +3,33 @@
 // Author: Stephanie He
 // Date: October 28, 2024
 
+// New Function that receives input from the user, sorts the letters,
+// and prints a new name.
+function sortYourName(){
+  var userName = window.prompt("Hello! Please give me your name so I can change it");
+  console.log("userName =", userName);
+
+  // Split the string to an array
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArraySort);
+
+  // Sort the array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+
+  // Join array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+
+  // Give the new name
+  return nameSorted;
+
+}
 
 
+// Output
+document.writeln("Here's your new name, I hope you like it: ",
+  sortYourName(), "</br>");
 
 
 
