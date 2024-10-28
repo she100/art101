c532f95626c064c338411ad7759eed9fa46b0e05
stxph <she100@ucsc.edu>
2024-10-27 22:56:40 -0700

    new folder yay

---
+++ lab7/js/lab.js
@@ -0,0 +1,29 @@
+// index.js - Lab 7
+// Write functions that take user input and manipulate a string.
+// Author: Stephanie He
+// Date: October 28, 2024
+
+
+
+
+
+
+
+
+// Stuff that came with the file automatically
+// Constants
+// Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main();
