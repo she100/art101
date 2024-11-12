dc60e908cce27279cb183929f76ac7cc5e0b3474
stxph <she100@ucsc.edu>
2024-11-11 17:38:35 -0800

    JS AND HTML

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -8,26 +8,40 @@
 // Function
 // Fake Dialogue
 
-function generateRandomDialogue() {
-  const text = "Hi! How are you? My name is Stephanie. And some other random words.";
-  const min = 3;
-  const max = 5;
-  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
-
-  // random starting index to slice the text
-  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
-  
-  // Generate random text
-  return text.slice(randStart, randStart + randLen);
-
+function generateRandomWords() {
+  const text = `Can you take a look inside your heart? Is there any room for me?
+I won't have to hold my breath 'til you get down on one knee because
+What's the point of crying? It was never even love
+Did you ever want me? Was I ever good enough?
+The-the boy's a liar, the boy's a liar
+He doesn't see ya, you're not looking at me, boy
+The boy's a liar, the boy's a liar
+He doesn't see ya, you're not looking at me, boy`;
+  const min = 2;
+  const max = 200;
+  const randLength = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index and adjust to nearest whitespace boundary
+  let randStart = Math.floor(Math.random() * (text.length - randLength + 1));
+  while (randStart > 0 && text[randStart - 1] !== ' ') {
+    randStart--;
+  }
+  // Set the end index and adjust to nearest whitespace boundary
+  let endIndex = randStart + randLength;
+  while (endIndex < text.length && text[endIndex] !== ' ') {
+    endIndex++;
+  }
+  // Generate the random text, breaking at the nearest whitespace boundaries
+  let selectedText = text.slice(randStart, endIndex).trim();
+  // Capitalize the first letter of the selected text
+  return selectedText;
 }
 
 
+// click listener for button
+$("#make-convo").click(function(){
+  // get new fake dialogue
+  const newText = generateRandomWords();
 
-
-
-
-
-
-
-
+  // append a new div to our output div
+  $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+});
