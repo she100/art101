9c3f4a1c0424c6064353523785a087c1cf97b41d
stxph <she100@ucsc.edu>
2024-11-12 15:45:07 -0800

    html

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,49 +1,26 @@
-// index.js - Lab 9
-// With a partner, experiment with jQuery
-// Modifies the elements on the page
+// index.js - Lab 11: JavaScript Events and forms
+// With a partner, experiment with JavaScript events.
 // Author: Stephanie He
-// Date: October 4, 2024
-
-// add challenge button to html
-$("#challenge").append("<button id='button-challenge'>Press me!!!</button>");
+// Date: November 9, 2024
 
 
-// challenge event listener
-$("#button-challenge").click(function(){
-  // console.log to show if the button is clickable when you press inspect -> console.
-  console.log("click");
-  // add or subtract the "special" class to the section
-  $("challenge").toggleClass("special");
+// Function for sorting a string, alphabetically
+function sortString(inputString) {
+  // Convert string to an array
+  // Then bring it back to sort it
+  return inputString.split('').sort().join('');
+}
 
+
+// Click listener for button
+$("#submit").click(function(){
+  // Get value of input field
+  const userName = $("#user-name").val();
+  // Sort the name using function
+  userNameSorted = sortString(userName);
+
+  return userNameSorted;
 });
 
-
-
-
-
-
-
-
-// click listener for challenge button
-//$("button-challenge").click(function(){
- //   $("#challenge").toggleClass("special");
-//});
-
-
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Adds a new div tag to our output div
+$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
