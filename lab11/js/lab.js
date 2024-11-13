d8f2dc063cb36606ac82c491dc5e276d24a7517d
stxph <she100@ucsc.edu>
2024-11-12 17:04:59 -0800

    js

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,7 +1,38 @@
 // index.js - Lab 11: JavaScript Events and forms
 // With a partner, experiment with JavaScript events.
 // Author: Stephanie He
-// Date: November 9, 2024
+// Date: November 11, 2024
+
+
+// From lab 10
+// Function that sorts a string
+function sortString(inputString) {
+  // Convert our string to an array and back again to sort it
+  return inputString.split('').sort().join('');
+}
+
+
+
+
+
+// Event listener for button
+// Click listener
+$("#submit").click(function(){
+  // get value of input field
+  const userName = $("#user-name").val();
+
+  // sort it
+  userNameSorted = sortString(userName);
+
+});
+
+
+// Select output div and append a new div, with modified user name
+$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+
+
+
+
 
 
 // Function for sorting a string, alphabetically
