2f7e76d45da312ad4fa9405b67ef0a10a036586e
stxph <she100@ucsc.edu>
2024-11-18 18:15:43 -0800

    js

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -4,116 +4,82 @@
 // Date: November 18, 2024
 
 
-// FUNCTION
-function sortingHat(str) {
-  // counts the letters in str
-  // then assigns to variable length
-  length = str.length;
+maxFactors = 4;
 
-  // modulus remainder % operator to get remainder with 4: mod = length % 4;
-  mod = length % 4;
-  // mod will now be a value between 0 and 3
-  if (mod == 0) {
-    return "Gryffindor house"
-  }
-  else if (mod == 1) {
-    return "Ravenclaw house"
-  }
-  else if (mod == 2) {
-    return "Slytherin house"
-  }
-  else if (mod == 3) {
-    return "Hufflepuff house"
-  }
-  // conditional that returns Gryffindor, Ravenclaw, Slytherin, & Hufflepuff
-  // depending on whether the value of mod is 0, 1, 2, or 3.
+outputEl = document.getElementById("output");
 
+// get the values from the webpage and write them in an object
+// this expects to have input fields with ids num0, text0, num1, text1, etc
+// returns an object that looks like this:
+//      {3: "Fizz", 5: "Buzz", 7: "Boom"}
+function getFactorObj() {
+    var factorObj = {};
+    for (var factor=0; factor<maxFactors; factor++) {
+        numId = "num" + factor;
+        textId = "text" + factor;
+        numValue = document.getElementById(numId).value;
+        textValue = document.getElementById(textId).value;
+        console.log(factor + ") num:", numValue, "text:", textValue)
+        // if either value is blank, don't use it
+        if (numValue && textValue) {
+            factorObj[numValue] = textValue;
+        }
+    }
+    return factorObj;
 }
 
-
-// USE THE STRING TO RETURN a hashed checksum of the string
-function checksum(s) {
-  var hash = 0, strlen = s.length, i, c;
-  if ( strlen === 0 ) {
-    return hash;
-  }
-  for ( i = 0; i < strlen; i++ ) {
-    c = s.charCodeAt( i );
-    hash = ((hash << 5) - hash) + c;
-    hash = hash & hash; // Convert to 32bit integer
-  }
-  return hash;
-};
-
-
-// Three pure javascript functions to manipulate classes
-// This is to take classes and sort them.
-function hasClass(ele,cls) {
-  return !!ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
-}
-//
-function addClass(ele,cls) {
-  if (!hasClass(ele,cls)) ele.className += " "+cls;
-}
-//
-function removeClass(ele,cls) {
-  if (hasClass(ele,cls)) {
-    var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
-    ele.className=ele.className.replace(reg,' ');
-  }
+function outputToPage(str) {
+    newEl = document.createElement("p");
+    newEl.innerHTML = str;
+    outputEl.appendChild(newEl);
 }
 
-// Given a name, hash the string, and
-// return Gryffindor, Ravenclaw, Slytherin, and Hufflepuff
-function sortingHatHash(str) {
-  checksumValue = checksum(str);
-  mod = Math.abs(checksumValue) % housesArray.length;
-  return housesArray[mod];      // returns an object from the array
+// given a number and an object that looks like this:
+//      {3: "Fizz", 5: "Buzz", 7: "Boom"}
+// loops over the numbers and outputs the number and the matching text
+// for factors
+function fizzBuzzBoom(maxNums, factorObj) {
+    // iterate over all of out numbers
+    for (var num=0; num<maxNums; num++) {
+        debugger;
+        // reset output string
+        var outputStr = "";
+        // iterate over the factors we got from the html
+        for (var factor in factorObj) {
+            // check to see if this num is a multiple of factor
+            if (num % factor == 0) {
+                // if yes, than add the text to output string
+                outputStr += factorObj[factor];
+            }
+        }
+        // now if we have words in outputStr, format it like this " - FizzBuzz!"
+        if (outputStr) {
+            outputStr = " - " + outputStr + "!";
+        }
+        outputToPage(num.toString() + outputStr)
+    }
 }
 
+function reportError(str) {
+    outputEl.innerHTML = "<div class='error'>" + str + "</div>";
+}
 
-// Click listener attached to #button
-// Declare the variable myButton, connected to "button" in html
-var myButton = document.getElementById("button");
-myButton.addEventListener("click", function(){
-
-  var houseObj = sortingHatHash(name);
-  var house = sortingHat(name);
-  newText = "<p>The Sorting Hat has sorted you into " + house + "</p>";
-  document.getElementById("output").innerHTML = newText;
-
+document.getElementById("submit").addEventListener("click", function() {
+    var max = document.getElementById("max").value;
+    console.log("max:", max)
+    if (! max) {
+        reportError("You must provide a maximum");
+        return;
+    }
+    var factorObj = getFactorObj();
+    console.log("factorObj:", factorObj);
+    if (Object.keys(factorObj).length === 0) {
+        reportError("You must provide at least one factor and text");
+        return;
+    }
+    // clear error if there is one
+    outputEl.innerHTML = "";
+    fizzBuzzBoom(max, factorObj);
+    outputEl.classList.add("cols");
 })
 
-$("button").click(function(){
-  var name = $("#input").val()
-  console.log(name);
-
-})
-
-
-
-
-
-
-housesArray = [
-  {
-    title: "Gryffindor",
-    text: "You have been assigned to the Gryffindor house."
-  },
-
-  {
-    title: "Hufflepuff",
-    text: "You have been assigned to the Hufflepuff house."
-  },
-
-  {
-    title: "Ravenclaw",
-    text: "You have been assigned to the Ravenclaw house."
-  },
-
-  {
-    title: "Slytherin",
-    text: "You have been assigned to the Slytherin house."
-  },
-
-]
